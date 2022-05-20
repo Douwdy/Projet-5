a8459e0195d2b0c74edbf757a1c484f66197a5f6
Douwdy <mrslim65.gl@gmail.com>
2022-05-20 13:18:39 +0200

    Adding Comments on the code

--- js/confirmation.js
+++ js/confirmation.js
@@ -1,3 +1,5 @@
+/* Getting the value of the parameter "commande" from the URL and putting it in the HTML element with
+the id "orderId". */
 const id = new URL(window.location.href).searchParams.get("commande");
 console.log(id);
 
